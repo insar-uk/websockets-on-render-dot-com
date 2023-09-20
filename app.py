7d28d07f237f261661fca738b8663af31ebe6cdf
insar-uk <79936153+insar-uk@users.noreply.github.com>
2023-09-20 18:22:10 +0100

    Works on render now. Local ports broken.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import logging
 
 LOCAL_URL: str = "0.0.0.0"
 LOCAL_PORT: int = 10000
@@ -8,12 +9,11 @@ async def echo(websocket, path):
     async for message in websocket:
         # Echo back the received message to the client
         print("Received message: " + message)
+        logging.info("Received message: " + message)
         await websocket.send(message)
 
 def main():
     print("Starting server...")
-
-    
     start_server = websockets.serve(echo, LOCAL_URL, LOCAL_PORT)
     
     # Create and run an asyncio event loop
